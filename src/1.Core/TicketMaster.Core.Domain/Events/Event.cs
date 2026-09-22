e75250b717e74702e718fabcb17efe25f6fa98ba
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-09-22 17:55:07 +0330

    -Edit Create function. -Add new functionalities to event.

--- src/1.Core/TicketMaster.Core.Domain/Events/Event.cs
+++ src/1.Core/TicketMaster.Core.Domain/Events/Event.cs
@@ -26,7 +26,7 @@ public class Event : AggregateRoot<int>
 
     public static Event Create(string title,
         EventType type,
-        EventStatus status,
+        DateTime dateTimeUtc,
         Duration durationMinute,
         Capacity capacity,
         Guid ownerId)
@@ -34,17 +34,71 @@ public class Event : AggregateRoot<int>
         Guard.ThrowExceptionIf.Empty(title, new DomainException(Error.Validation(parameters: [nameof(title)])));
         Guard.ThrowExceptionIf.Null(durationMinute, new DomainException(Error.Validation(parameters: [nameof(durationMinute)])));
         Guard.ThrowExceptionIf.Null(capacity, new DomainException(Error.Validation(parameters: [nameof(capacity)])));
+        Guard.ThrowExceptionIf.LessThanOrEqual(dateTimeUtc, DateTime.UtcNow, new DomainException(Error.Validation(parameters: [nameof(dateTimeUtc)])));
 
         return new()
         {
             Title = title,
             Type = type,
-            Status = status,
-            DateTimeUtc = DateTime.UtcNow,
+            Status = EventStatus.Draft,
+            DateTimeUtc = dateTimeUtc,
             DurationMinute = durationMinute,
             Capacity = capacity,
             OwnerId = ownerId
         };
     }
-    public bool IsAvailable() => Status == EventStatus.OnSale;
+
+    public bool IsAvailable() => Status == EventStatus.OnSale && DateTimeUtc > DateTime.UtcNow;
+
+    public void Sell()
+    {
+        Guard.ThrowExceptionIf.NotEqual(Status, EventStatus.Draft,
+            new DomainException(Error.Failure(description: "Event must be in draft status for stating selling.",
+            parameters: [nameof(Sell)])));
+
+        Guard.ThrowExceptionIf.LessThan(DateTimeUtc, DateTime.UtcNow,
+            new DomainException(Error.Failure(description: "Event's time is over for setting to on sale.",
+            parameters: [nameof(Sell)])));
+
+        Status = EventStatus.OnSale;
+    }
+
+    public void SellOut(int reservedCount)
+    {
+        if (!IsAvailable())
+            throw new DomainException(Error.Failure(description: "Event isn't available.",
+                parameters: [nameof(SellOut)]));
+
+        if (Capacity.Value > reservedCount)
+            throw new DomainException(Error.Failure(description: "Event's had available tickets.",
+                parameters: [nameof(SellOut)]));
+
+        Status = EventStatus.SoldOut;
+    }
+
+    public void Cancel()
+    {
+        Guard.ThrowExceptionIf.Equal(Status, EventStatus.Finished,
+            new DomainException(Error.Failure(description: "Event with finished status can't be cancelled.",
+            parameters: [nameof(Cancel)])));
+
+        Guard.ThrowExceptionIf.LessThan(DateTimeUtc, DateTime.UtcNow,
+            new DomainException(Error.Failure(description: "Event's time is over for setting to cancelled.",
+            parameters: [nameof(Cancel)])));
+
+        Status = EventStatus.Cancelled;
+    }
+
+    public void Finish()
+    {
+        Guard.ThrowExceptionIf.Equal(Status, EventStatus.Finished,
+            new DomainException(Error.Failure(description: "Event's status must be on sale on.",
+            parameters: [nameof(Finish)])));
+
+        Guard.ThrowExceptionIf.LessThan(DateTimeUtc, DateTime.UtcNow,
+            new DomainException(Error.Failure(description: "Event's date time must be grater than current date time.",
+            parameters: [nameof(Cancel)])));
+
+        Status = EventStatus.Finished;
+    }
 }
