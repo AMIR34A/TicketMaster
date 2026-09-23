a408cc42c8ae65afe5388fbf8a9a10c110cc84b2
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-09-23 17:16:41 +0330

    Add GetTicketsCountByEventIdAsync to TicketRepository.

---
+++ src/1.Core/TicketMaster.Core.Contracts/Data/Repositories/ITicketRepository.cs
@@ -0,0 +1,8 @@
+﻿using TicketMaster.Core.Domain.Tickets;
+
+namespace TicketMaster.Core.Contracts.Data.Repositories;
+
+public interface ITicketRepository : IBaseRepository<Ticket, int>, IUnitOfWork
+{
+    ValueTask<int> GetTicketsCountByEventIdAsync(int eventId, CancellationToken cancellationToken = default);
+}
---
+++ src/2.Infrastructure/TicketMaster.Infrastructure.Data/Repositories/TicketRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketMaster.Core.Contracts.Data.Repositories;
+using TicketMaster.Core.Domain.Tickets;
+
+namespace TicketMaster.Infrastructure.Data.Repositories;
+
+internal class TicketRepository : BaseRepository<Ticket, TicketMasterDbContext, int>, ITicketRepository
+{
+    public TicketRepository(TicketMasterDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async ValueTask<int> GetTicketsCountByEventIdAsync(int eventId,
+        CancellationToken cancellationToken = default) => await _dbContext.Set<Ticket>()
+        .Where(t => t.EventId == eventId)
+        .CountAsync(cancellationToken);
+}
