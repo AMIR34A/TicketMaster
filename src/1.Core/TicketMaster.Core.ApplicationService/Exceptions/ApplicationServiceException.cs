e31a6ca9da24f727a7282908e99450be494f8faf
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-09-23 09:19:43 +0330

    Add ApplicationServiceException.

---
+++ src/1.Core/TicketMaster.Core.ApplicationService/Exceptions/ApplicationServiceException.cs
@@ -0,0 +1,10 @@
+﻿using TicketMaster.Shared.Utilities.Exceptions;
+
+namespace TicketMaster.Core.ApplicationService.Exceptions;
+
+public class ApplicationServiceException : BaseException
+{
+    public ApplicationServiceException(Error error) : base(error)
+    {
+    }
+}
