2097599ea1c7b59959a0625cdb3cef8b9071f998
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-09-24 10:32:53 +0330

    Refactor GetTicketsCountByEventIdAsync method.

--- src/2.Infrastructure/TicketMaster.Infrastructure.Data/Repositories/TicketRepository.cs
+++ src/2.Infrastructure/TicketMaster.Infrastructure.Data/Repositories/TicketRepository.cs
@@ -4,7 +4,7 @@ using TicketMaster.Core.Domain.Tickets;
 
 namespace TicketMaster.Infrastructure.Data.Repositories;
 
-internal class TicketRepository : BaseRepository<Ticket, TicketMasterDbContext, int>, ITicketRepository
+public class TicketRepository : BaseRepository<Ticket, TicketMasterDbContext, int>, ITicketRepository
 {
     public TicketRepository(TicketMasterDbContext dbContext) : base(dbContext)
     {
@@ -12,6 +12,5 @@ internal class TicketRepository : BaseRepository<Ticket, TicketMasterDbContext, 
 
     public async ValueTask<int> GetTicketsCountByEventIdAsync(int eventId,
         CancellationToken cancellationToken = default) => await _dbContext.Set<Ticket>()
-        .Where(t => t.EventId == eventId)
-        .CountAsync(cancellationToken);
+        .CountAsync(t => t.EventId == eventId, cancellationToken);
 }
